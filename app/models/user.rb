94f4d5faa31faa56983c9f1e68994cd274f1b958
Aleksey Zaytsev <drshnitsel@gmail.com>
2015-02-14 14:02:56 +0300

    Collect user nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.name = auth.name
+      user.nickname = auth.nickname
       user.avatar = auth.image
     end
   end
