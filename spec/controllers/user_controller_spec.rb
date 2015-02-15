85ce38e9eab8e738ad0ba46145e8784a772a8801
Aleksey Zaytsev <drshnitsel@gmail.com>
2015-02-15 18:27:50 +0300

    fix controllers tests

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe UserController, type: :controller do
 
   describe "GET #recommendations" do
     it "returns http success" do
+      user = FactoryGirl.create(:user)
+      sign_in user
       get :recommendations
       expect(response).to have_http_status(:success)
     end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@ require 'rspec/rails'
 OmniAuth.config.test_mode = true
 OmniAuth.config.mock_auth[:steam] = OmniAuth::AuthHash.new(
       provider: 'steam',
-      uid: '1234',
+      uid: '76561198028177455',
       name: 'Тест Тестерович',
       nickname: 'TheTestingGuy',
       image: 'http://png-1.findicons.com/files/icons/1072/face_avatars/300/i04.png'
