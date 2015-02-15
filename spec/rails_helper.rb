e03a46578289aa1f155296a4947da1c8222bf784
Aleksey Zaytsev <drshnitsel@gmail.com>
2015-02-15 18:29:36 +0300

    fix omniauth mock

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,9 +8,11 @@ OmniAuth.config.test_mode = true
 OmniAuth.config.mock_auth[:steam] = OmniAuth::AuthHash.new(
       provider: 'steam',
       uid: '76561198028177455',
-      name: 'Тест Тестерович',
-      nickname: 'TheTestingGuy',
-      image: 'http://png-1.findicons.com/files/icons/1072/face_avatars/300/i04.png'
+      info: {
+        name: 'Тест Тестерович',
+        nickname: 'TheTestingGuy',
+        image: 'http://png-1.findicons.com/files/icons/1072/face_avatars/300/i04.png'
+      }
   )
 
 # Add additional requires below this line. Rails is not loaded until this point!
